079c5340e10c24943ea4e173cc89cf43cab7e304
Sumit Singh Kushwaha <68112081+sumitsinghkushwaha@users.noreply.github.com>
2023-12-06 22:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// complete the given function
+function palindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
-function palindrome(str){
-
+  // Compare the original and reversed strings
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
 }
-module.exports = palindrome
+
+// Example usage
+console.log(palindrome("race a car")); // Output: false
+console.log(palindrome("racecar"));    // Output: true
+console.log(palindrome("RaceCar"));    // Output: true
+console.log(palindrome("A man, a plan, a canal, Panama")); // Output: true
